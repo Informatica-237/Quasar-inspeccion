34946eb10ddd5656916d422028df04f9e9ee92d8
Informatica-237 <pruebasinformatica237@gmail.com>
2024-09-27 11:31:08 -0300

    datos

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,20 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-
-    <q-header elevated>
-      <div class="q-pa-md q-gutter-sm">
-        <q-breadcrumbs align="center" active-color="white" style="font-size: 16px">
-          <q-breadcrumbs-el label="INFRACCION" icon="car_crash" to="/infraccion"/>
-          <q-breadcrumbs-el label="ACTA DE INFRACCION" icon="post_add" to="/acta"/>
+    <q-header elevated class="custom-header" >
+      <div class="q-pa-md q-gutter-sm"  >
+        <q-breadcrumbs align="center" active-color="white" style="font-size: 20px; color: white;">
+          <q-breadcrumbs-el
+            label="INFRACCION"
+            icon="car_crash"
+            :to="{ path: '/infraccion' }"
+            :class="{ 'active-breadcrumb': route.path === '/infraccion' }"
+          />
+          <q-breadcrumbs-el
+            label="ACTA DE INFRACCION"
+            icon="post_add"
+            :to="{ path: '/acta' }"
+            :class="{ 'active-breadcrumb': route.path === '/acta' }"
+          />
         </q-breadcrumbs>
       </div>
     </q-header>
@@ -13,13 +22,23 @@
     <q-page-container>
       <router-view></router-view>
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 
-defineOptions({
-  name: 'MainLayout'
-});
+const route = useRoute(); // Obtener la ruta actual
 </script>
+
+<style scoped>
+.active-breadcrumb {
+  font-weight: bold;
+  color: #000000; /* Cambia el color del texto activo a negro */
+  /* Eliminamos la sombra */
+}
+
+.custom-header {
+  background-color: rgb(110, 185, 255); /* Cambia a cualquier color que desees */
+}
+</style>
--- src/pages/Infraccion.vue
+++ src/pages/Infraccion.vue
@@ -1,186 +1,213 @@
 <template>
   <div>
 
-    <div class="row justify-center">
-      <h2><b>INFRACCION</b></h2>
-    </div>
-
-    <div class="row justify-center q-gutter-sm">
-      <q-input
-        v-model="search"
-        debounce="1000"
-        filled
-        placeholder=""
-      >
+    <div class="row justify-center q-gutter-md" style="margin-top: 20px;">
+      <q-input v-model="transitoStore.busquedaInfracciones" debounce="500" filled label="Buscar"
+        style="min-width: 550px;">
         <template v-slot:append>
           <q-icon name="search" />
         </template>
       </q-input>
-      <q-btn color="primary" label="Buscar" />
+
+      <q-btn color="light-blue-5" label="NUEVA INFRACCION" icon="health_and_safety" @click="openModal" />
     </div>
 
     <div class="row justify-center">
-      <q-btn color="primary" label="Modal" @click="openModal"/>
-    </div>
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card" v-for="infraccion in transitoStore.infraccionesFiltradas" :key="infraccion.id">
+          <q-card-section>
 
-    <q-dialog
-    v-model="fullHeight"
-    full-height
-    full-width
-    persistent
-    >
-    <q-card class="column full-height" >
-      <q-card-section>
-        <div class="row justify-between q-gutter-sm">
-          <div class="text-h6">Full Height</div>
-          <q-btn icon="close" flat round dense v-close-popup />
-        </div>
-      </q-card-section>
+            <div class="row">
+            <div class="col-md-4">
+              <div class="text-h6">FECHA:</div>
+              <div class="text-subtitle2">{{ infraccion.fechaHora }}</div>
+            </div>
+            </div>
+            
+            <div class="row">
+              <div class="col-md-4">
+                <div class="text-h6">NOMBRE:</div>
+                <div class="text-subtitle2">{{ infraccion.nombre }}</div>
+              </div>
+            </div>
 
-      <q-card-section class="col q-pt-none scroll">
+            <div class="row">
+            <div class="col-md-4">
+              <div class="text-h6">DNI:</div>
+              <div class="text-subtitle2">{{ infraccion.licenciaConducir }}</div>
+            </div>
+            </div>
 
 
-        <div class="row justify-center">
-      <h4><b>FECHA Y HORA</b></h4>
-    </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <div class="q-gutter-md row items-start">
-        <q-date v-model="fecha1" mask="YYYY-MM-DD HH:mm" color="primary" />
-        <q-time v-model="fecha2" mask="YYYY-MM-DD HH:mm" color="primary" />
+          </q-card-section>
+        </q-card>
       </div>
     </div>
 
-    <div class="row justify-center">
-      <h4><b>CONDUCTOR</b></h4>
-    </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="nombre" label="NOMBRE" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="apellido" label="APELLIDO" />
-      <div class="col-md-2">
-        <q-option-group v-model="checkboxes" :options="checkboxOptions" type="radio" inline color="primary" />
-      </div>
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="domicilio" label="DOMICILIO" />
-    </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="localidad" label="LOCALIDAD" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="codigopostal" label="CODIGO POSTAL" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="partido" label="PARTIDO" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="provincia" label="PROVINCIA" />
-    </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="pais" label="PAIS" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="licenciaconducir" label="LICENCIA DE CONDUCIR" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="clase" label="CLASE" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="vencimiento" label="VENCIMIENTO" />
-    </div>
+    <q-dialog v-model="fullHeight" full-height full-width persistent>
+      <q-card class="column full-height">
+        <q-card-section>
+          <div class="row justify-between q-gutter-sm">
+            <div class="text-h6">Full Height</div>
+            <q-btn icon="close" flat round dense v-close-popup />
+          </div>
+        </q-card-section>
+        <q-card-section class="col q-pt-none scroll">
+          <q-scroll-area style="  display: flex;
+  flex-direction: column;
+  height: 100%;">
+            <div class="q-py-xs">
 
-    <div class="row justify-center">
-      <H6>TIPO DOCUMENTO</H6>
-    </div>
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-select standout="bg-primary text-black" style="min-width: 200px; max-width: 300px" v-model="opcionesdni"
-      :options="optiones" label="TIPO" />
 
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="espaciodni" label="" />
-    </div>
+              <div class="row justify-center">
+                <h4><b>FECHA Y HORA</b></h4>
+              </div>
 
-    <div class="row justify-center">
-      <h3><b>VEHICULO</b></h3>
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <div class="q-gutter-md row items-start">
+                  <q-date v-model="fecha1" mask="YYYY-MM-DD HH:mm" color="primary" />
+                  <q-time v-model="fecha2" mask="YYYY-MM-DD HH:mm" color="primary" />
+                </div>
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-select standout="bg-primary text-black" style="min-width: 200px; max-width: 300px" v-model="model"
-      :options="options" label="TIPO" />
-      <q-select standout="bg-primary text-black" style="min-width: 200px; max-width: 300px" v-model="model2"
-      :options="options2" label="MARCA" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="otramarca" label="OTRA MARCA" :disable="model2 !== 'OTRO'"/>
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="modelo" label="MODELO" />
-    </div>
+              <div class="row justify-center">
+                <h4><b>CONDUCTOR</b></h4>
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="color" label="COLOR" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="dominio" label="Nº DOMINIO" />
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="nombre" label="NOMBRE" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="apellido" label="APELLIDO" />
+                <div class="col-md-2">
+                  <q-option-group v-model="checkboxes" :options="checkboxOptions" type="radio" inline color="primary" />
+                </div>
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="domicilio" label="DOMICILIO" />
+              </div>
 
-    <div class="row justify-center">
-      <h4><b>HECHO QUE CONFIGURA LA INFRACCION Y LUGAR</b></h4>
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="localidad" label="LOCALIDAD" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="codigopostal"
+                  label="CODIGO POSTAL" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="partido" label="PARTIDO" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="provincia" label="PROVINCIA" />
+              </div>
 
-    <div class="row justify-center">
-      <H6>DISPOSICION LEGAL INFRIGIDA LEY</H6>
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="pais" label="PAIS" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="licenciaconducir"
+                  label="LICENCIA DE CONDUCIR" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="clase" label="CLASE" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="vencimiento" label="VENCIMIENTO" />
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="ley" label="" />
-    </div>
+              <div class="row justify-center">
+                <H6>TIPO DOCUMENTO</H6>
+              </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-select standout="bg-primary text-black" style="min-width: 200px; max-width: 300px"
+                  v-model="opcionesdni" :options="optiones" label="TIPO" />
 
-    <div class="row justify-center">
-      <h4><b>OBSERVACIONES / DESCRIPCION / TESTIGOS / PRUEBA DOCUMENTAL</b></h4>
-    </div>
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="espaciodni" label="" />
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="observaciones" label="OBSERVACIONES" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="descripcion" label="DESCRIPCION" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="testigos" label="TESTIGOS" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="pruebadocumental" label="PRUEBA DOCUMENTAL" />
-    </div>
+              <div class="row justify-center">
+                <h3><b>VEHICULO</b></h3>
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="lugarinfraccion" label="LUGAR INFRACCION" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="localidad" label="LOCALIDAD" />
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-select standout="bg-primary text-black" style="min-width: 200px; max-width: 300px" v-model="model"
+                  :options="options" label="TIPO" />
+                <q-select standout="bg-primary text-black" style="min-width: 200px; max-width: 300px" v-model="model2"
+                  :options="options2" label="MARCA" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="otramarca" label="OTRA MARCA"
+                  :disable="model2 !== 'OTRO'" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="modelo" label="MODELO" />
+              </div>
 
-    <div class="row justify-center">
-      <h4><b>ESPECIFICACIONES DEL CINEMOMETRO / ALCOHOLIMETRO</b></h4>
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="color" label="COLOR" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="dominio" label="Nº DOMINIO" />
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="cinometro" label="" />
-      <q-input class="col-md-2" standout="bg-primary text-white" v-model="alcoholimetro" label="" />
-    </div>
+              <div class="row justify-center">
+                <h4><b>HECHO QUE CONFIGURA LA INFRACCION Y LUGAR</b></h4>
+              </div>
 
-    <div class="row justify-center">
-      <h4><b>MEDIDAS PRECAUTORIAS</b></h4>
-    </div>
+              <div class="row justify-center">
+                <H6>DISPOSICION LEGAL INFRIGIDA LEY</H6>
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-      <h6>RETUVO LICENCIA</h6>
-      <div class="col-md-2">
-        <q-option-group v-model="checkboxes2" :options="checkboxOptions2" type="radio" inline color="primary" />
-      </div>
-      <h6>RETUVO VEHICULO</h6>
-      <div class="col-md-2">
-        <q-option-group v-model="checkboxes3" :options="checkboxOptions3" type="radio" inline color="primary" />
-      </div>
-    </div>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="ley" label="" />
+              </div>
 
-    <div class="row q-pa-md q-gutter-lg justify-center">
-    <q-btn color="primary" label="Enviar" />
-    </div>
+              <div class="row justify-center">
+                <h4><b>OBSERVACIONES / DESCRIPCION / TESTIGOS / PRUEBA DOCUMENTAL</b></h4>
+              </div>
 
-    
-  </q-card-section>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="observaciones"
+                  label="OBSERVACIONES" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="descripcion" label="DESCRIPCION" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="testigos" label="TESTIGOS" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="pruebadocumental"
+                  label="PRUEBA DOCUMENTAL" />
+              </div>
 
-</q-card>
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="lugarinfraccion"
+                  label="LUGAR INFRACCION" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="localidad" label="LOCALIDAD" />
+              </div>
+
+              <div class="row justify-center">
+                <h4><b>ESPECIFICACIONES DEL CINEMOMETRO / ALCOHOLIMETRO</b></h4>
+              </div>
+
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="cinometro" label="" />
+                <q-input class="col-md-2" standout="bg-primary text-white" v-model="alcoholimetro" label="" />
+              </div>
+
+              <div class="row justify-center">
+                <h4><b>MEDIDAS PRECAUTORIAS</b></h4>
+              </div>
+
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <h6>RETUVO LICENCIA</h6>
+                <div class="col-md-2">
+                  <q-option-group v-model="checkboxes2" :options="checkboxOptions2" type="radio" inline
+                    color="primary" />
+                </div>
+                <h6>RETUVO VEHICULO</h6>
+                <div class="col-md-2">
+                  <q-option-group v-model="checkboxes3" :options="checkboxOptions3" type="radio" inline
+                    color="primary" />
+                </div>
+              </div>
+
+              <div class="row q-pa-md q-gutter-lg justify-center">
+                <q-btn color="primary" label="Enviar" />
+              </div>
+
+            </div>
+          </q-scroll-area>
+
+
+        </q-card-section>
+
+      </q-card>
     </q-dialog>
-
-
-
-
-
-
-
-
 
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { useTransitoStore } from 'stores/transitoStore'; // Asegúrate de que esté disponible el store
 
 const nombre = ref('');
 const apellido = ref('');
@@ -211,7 +238,7 @@ const opcionesdni = ref('');
 const espaciodni = ref('');
 
 
-const search = ref('');
+
 
 
 const fullHeight = ref(false); // Estado para el modal
@@ -258,7 +285,18 @@ const optiones = [
   'DNI', 'LE', 'LC', 'PAS', 'CI'
 ]
 
+
 defineOptions({
   name: 'infraccionPage'
 });
+
+
+const transitoStore = useTransitoStore();
+
+
+onMounted(() => {
+  // Cargar los datos cuando se monte el componente
+  transitoStore.cargarInfracciones();
+});
+
 </script>
