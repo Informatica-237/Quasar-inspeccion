76b27239f3dafea93cf1ddb93afc253f3c840c05
EnzoSivesTelpin <158466882+EnzoSivesTelpin@users.noreply.github.com>
2024-09-30 21:27:23 -0300

    Mejoras

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,18 +6,18 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '/infraccion', component: () => import('pages/Infraccion.vue') },
-      { path: '/acta', component: () => import('pages/Acta.vue') }
+      { path: '/acta', component: () => import('pages/Acta.vue') },
+      { path: '/', component: () => import('pages/homePage.vue') },
     ],
   },
-  {
-    path: '/home',
-    component: () => import('pages/homePage.vue'),
-  },
+  // {
+  //   path: '/home',
+  //   component: () => import('pages/homePage.vue'),
+  // },
   {
     path: '/index',
     component: () => import('src/pages/IndexPage.vue'),
   },
- 
 
   // Siempre deja esto al final,
   // pero lo puedes remover si es necesario
