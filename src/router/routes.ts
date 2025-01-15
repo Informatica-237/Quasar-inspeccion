0d225c53dbae5fa7e488aebbced947d8445ad92b
EnzoSivesTelpin <158466882+EnzoSivesTelpin@users.noreply.github.com>
2025-01-14 22:34:13 -0300

    Arreglos en meta

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,7 +8,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/index',
     component: () => import('layouts/MainLayout.vue'),
-    meta: { requiresAuth: false },
+    meta: { requiresAuth: true },
     children: [
       { path: '/infraccion', component: () => import('pages/Infraccion.vue') },
       { path: '/acta', component: () => import('pages/Acta.vue') },
